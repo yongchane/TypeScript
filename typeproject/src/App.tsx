4e0cc71e7da9a1ec9b99e540dfe2ee1fd0dfd2b7
yongchane <202021113@sangmyung.kr>
2024-08-18 23:19:59 +0900

    #1  함수형 컴포넌트를 타입스크립트로 작성하rl

---
+++ typeproject/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Greetings from "./Greetings";
+
+const App = () => {
+  const onClick = (name: string) => {
+    console.log(`${name} says hello`);
+  };
+  return <Greetings name="Hello" onClick={onClick} />;
+  // props 를 빠뜨리게 된다면 에디터에 오류 발생
+};
+
+export default App;
