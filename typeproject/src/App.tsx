92d313600c5be34b971496a6588a86b6bb41fd01
yongchane <202021113@sangmyung.kr>
2024-08-18 23:42:08 +0900

    #2 타입스크립트로 리액트 상태 관리하기
    인풋 상태 관리 부분부터 공부 필요

--- typeproject/src/App.tsx
+++ typeproject/src/App.tsx
@@ -2,13 +2,15 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Greetings from "./Greetings";
+import Counter from "./Counter";
 
 const App = () => {
-  const onClick = (name: string) => {
-    console.log(`${name} says hello`);
-  };
-  return <Greetings name="Hello" onClick={onClick} />;
+  // const onClick = (name: string) => {
+  //   console.log(`${name} says hello`);
+  // };
+  // return <Greetings name="Hello" onClick={onClick} />;
   // props 를 빠뜨리게 된다면 에디터에 오류 발생
+  return <Counter />;
 };
 
 export default App;
---
+++ typeproject/src/Counter.tsx
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+
+function Counter() {
+  //useState 를 사용하실때 useState<number>() 와 같이 Generics 를 사용하여 해당 상태가 어떤 타입을 가지고 있을지 설정
+  //useState를 사용 할 때 Generics 를 사용하지 않아도 알아서 타입을 유추
+  //useState사용할 때null일 수도 있고 아닐수도 있을때 Generics 를 활용
+  const [count, setCount] = useState<number>(0);
+  const onIncrease = () => setCount(count + 1);
+  const onDecrease = () => setCount(count - 1);
+  return (
+    <div>
+      <h1>{count}</h1>
+      <div>
+        <button onClick={onIncrease}>+1</button>
+        <button onClick={onDecrease}>-1</button>
+      </div>
+    </div>
+  );
+}
+
+export default Counter;
