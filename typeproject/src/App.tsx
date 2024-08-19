d3707bd213058f6894edecc473962fe5ed056d63
yongchane <202021113@sangmyung.kr>
2024-08-19 23:26:46 +0900

    #2 useState,인풋 사용하는 법

--- typeproject/src/App.tsx
+++ typeproject/src/App.tsx
@@ -3,6 +3,7 @@ import logo from "./logo.svg";
 import "./App.css";
 import Greetings from "./Greetings";
 import Counter from "./Counter";
+import MyForm from "./MyForm";
 
 const App = () => {
   // const onClick = (name: string) => {
@@ -10,7 +11,10 @@ const App = () => {
   // };
   // return <Greetings name="Hello" onClick={onClick} />;
   // props 를 빠뜨리게 된다면 에디터에 오류 발생
-  return <Counter />;
+  const onSubmit = (form: { name: string; description: string }) => {
+    console.log(form);
+  };
+  return <MyForm onSubmit={onSubmit} />;
 };
 
 export default App;
--- typeproject/src/Counter.tsx
+++ typeproject/src/Counter.tsx
@@ -4,6 +4,8 @@ function Counter() {
   //useState 를 사용하실때 useState<number>() 와 같이 Generics 를 사용하여 해당 상태가 어떤 타입을 가지고 있을지 설정
   //useState를 사용 할 때 Generics 를 사용하지 않아도 알아서 타입을 유추
   //useState사용할 때null일 수도 있고 아닐수도 있을때 Generics 를 활용
+  //type Information = { name: string; description: string };
+  //const [info, setInformation] = useState<Information | null>(null);
   const [count, setCount] = useState<number>(0);
   const onIncrease = () => setCount(count + 1);
   const onDecrease = () => setCount(count - 1);
