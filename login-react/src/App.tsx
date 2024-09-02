c9c8ab5dbeb9dcd620df40d65b8172d76bea3da0
yongchane <202021113@sangmyung.kr>
2024-09-02 23:56:23 +0900

    #8 hook에 타입 적용하기

--- login-react/src/App.tsx
+++ login-react/src/App.tsx
@@ -6,16 +6,24 @@ import Label from "./components/Label";
 import { useSetLocale, useTranslate } from "./translate";
 
 function App() {
-  const [values, setValues] = useState({
+  // 보통 useState는 타입을 선언할 필요가 없지만 예를 들어 문자열 배열을 state로 만든다고 했을떄
+  //빈배열을 초깃값을 쓰면 이런경우에는 타입을 추론할 수 없으니 state 타입이 never(절때로 있을 수 없는 값)이라는 타입의 배열이된다
+  //이떄 제너릭을 통해 아래와 같이 타입을 작성한다
+  const [values, setValues] = useState<{ username: string; password: string }>({
     username: "",
     password: "",
   });
-  const formRef = useRef();
+  //Ref로 HTML DOM 노드를 가져와서 쓸거라면 제네릭으로 HTML DOM 노드에 해당하는 타입을 적어 주면 된다
+  // 만약 mutablerefobject htmlformelement|undefined라고 명시되면 초깃값으로 null이라고 적어주면 됨
+  // 즉 초깃값이 비어 있으면 추론된 Ref 객체 타입과 Ref를 사용하는 곳의 타입이 맞지 않기 때문에 초깃값으로 null을 지정해 주면 된다
+  //useRef에서는 제네릭으로 대상이 되는 노드의 타입을 정해 줄 수 있다
+  const formRef = useRef<HTMLFormElement>(null);
   const t = useTranslate();
   const setLocale = useSetLocale();
 
+  //useEffect훅 같은 경우에는 타입을 정의할 일이 거의 없음
   useEffect(() => {
-    const form: any = formRef.current;
+    const form = formRef.current;
     if (form) form["username"].focus();
   }, []);
 
@@ -36,7 +44,7 @@ function App() {
   }
 
   return (
-    <form className="login" ref={formRef as any}>
+    <form className="login" ref={formRef}>
       <h1 className="login-title">{t("signin")}</h1>
       <Label>{t("username")}</Label>
       <Input
--- login-react/src/components/Input.tsx
+++ login-react/src/components/Input.tsx
@@ -6,6 +6,9 @@ interface Props extends InputHTMLAttributes<HTMLInputElement> {}
 //제네릭의 타입 파라미터로는 DOM 노드에 해당하는 타입을 적어줌
 // className 같은 것도 HTMLAttributes에 포함되어있으니 생략해도 좋음
 // 이런식으로 HTML 태그에서 쓰는 기본 Prop을 가져올 수 있다
+//HTML 기본 속성으로 PropType을 지정하고 싶을때HTMLAttributes로 끝나는 타입을 써서제네릭 안에는(<>)
+//HTML DOM 노드에 해당하는 타입을 넣은 다음 해당 타입을 상속(extends)해서 사용가능
+
 export default function Input({ className = "", ...rest }: Props) {
   const classNames = `${styles.input} ${className}`;
   return <input className={classNames} {...rest} />;
